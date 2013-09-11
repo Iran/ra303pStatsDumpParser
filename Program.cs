7308939cafce91e33268f486418572fb2f9739e7
iran <ma1kelvdh@gmail.com>
2013-09-11 16:47:08 +0200

    update solution and target .Net framework v2

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using ra303pStatsDumpParser;
 
 namespace ra303pStatsDumpReader
