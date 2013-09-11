44d9276844bc4935dc253e7b26692a536d49012f
iran <ma1kelvdh@gmail.com>
2013-09-11 15:09:09 +0200

    Finish support for everything except parsing unit info

--- StatsDumpParser.cs
+++ StatsDumpParser.cs
@@ -21,6 +21,8 @@ namespace ra303pStatsDumpParser
         int[] PlayerMoneyHarvested;
         int[] PlayerCredits;
         int[] PlayerQuitStates;
+        int[] PlayerColors;
+        string[] PlayerNames;
         string[] PlayerSides;
         CratesCollectedStruct[] PlayerCratesCollected;
         int SDFX = -1;
@@ -50,6 +52,7 @@ namespace ra303pStatsDumpParser
         uint VideoMemory = 0;
         int GameSpeed = -1;
         string Version = "UNPARSED";
+        DateTime? GameDateTimeUTC = null; // null is value if not parsed
 
 
         public StatsDumpParser(string FileName)
@@ -57,10 +60,12 @@ namespace ra303pStatsDumpParser
             PlayerMoneyHarvested = new int[] { -1, -1, -1, -1, -1, -1, -1, -1 };
             PlayerCredits = new int[] { -1, -1, -1, -1, -1, -1, -1, -1 };
             PlayerQuitStates = new int[] { -1, -1, -1, -1, -1, -1, -1, -1 };
+            PlayerColors = new int[] { -1, -1, -1, -1, -1, -1, -1, -1 };
             PlayerCratesCollected = new CratesCollectedStruct[8];
             PlayerSides = new string[8];
+            PlayerNames = new string[8];
 
-            this.Parse_Stats_Dump_File(FileName);   
+            this.Parse_Stats_Dump_File(FileName);
         }
 
         public void Parse_Stats_Dump_File(string FileName)
@@ -74,7 +79,7 @@ namespace ra303pStatsDumpParser
                 this.DumpSize = (int)Bin.BaseStream.Length;
 
                 // Size needs to be at least 4 bytes for the size header in the dump file
-                if (this.DumpSize< 4 )
+                if (this.DumpSize < 4)
                 {
                     throw new StatsDumpLengthException();
                 }
@@ -88,12 +93,22 @@ namespace ra303pStatsDumpParser
                     Byte[] Bytes = Bin.ReadBytes(4); Pos += 4;
                     string ID = System.Text.Encoding.Default.GetString(Bytes);
 
-                    if (ID.Contains("SID"))
+                    if (ID.Contains("NAM"))
+                    {
+                        Parse_Name_Info(ID);
+                    }
+
+                    if (ID.Contains("COL"))
+                    {
+                        Parse_Color_Info(ID);
+                    }
+
+                    else if (ID.Contains("SID"))
                     {
                         Parse_Side_Info(ID);
                     }
 
-                    if ( ID.Contains("HRV") )
+                    else if (ID.Contains("HRV"))
                     {
                         Bin.ReadBytes(4);
                         int Money = Bin.ReadInt32();
@@ -242,6 +257,10 @@ namespace ra303pStatsDumpParser
                     {
                         this.Parse_Quit_State();
                     }
+                    else if (ID == "DATE")
+                    {
+                        this.Parse_Date_Info();
+                    }
                 }
             }
         }
@@ -277,11 +296,14 @@ namespace ra303pStatsDumpParser
             Console.WriteLine("VideoMemory = {0}", this.VideoMemory);
             Console.WriteLine("GameSpeed = {0}", this.GameSpeed);
             Console.WriteLine("Version = {0}", this.Version);
+            Console.WriteLine("GameDateTimeUTC = {0}", this.GameDateTimeUTC.ToString());
 
             this.Print_Player_Array(this.PlayerMoneyHarvested, "Money harvested for player {0} = {1}");
             this.Print_Player_Array(this.PlayerCredits, "Credits for player {0} = {1}");
             this.Print_Player_Array(this.PlayerQuitStates, "Quit state for player {0} = {1}");
+            this.Print_Player_Array(this.PlayerColors, "Color for player {0} = {1}");
             this.Print_Player_String_Array(this.PlayerSides, "Side for player {0} = {1}");
+            this.Print_Player_String_Array(this.PlayerNames, "Name for player {0} = {1}");
         }
 
         public void Parse_Quit_State()
@@ -292,10 +314,29 @@ namespace ra303pStatsDumpParser
             this.PlayerQuitStates[PlayerNum - 1] = this.Read_Byte();
         }
 
+        public void Parse_Date_Info()
+        {
+            Read_Garbage();
+            FileTime FTime = new FileTime();
+
+            FTime.dwLowDateTime = this.Read_Unsigned_32Bits();
+            FTime.dwHighDateTime = this.Read_Unsigned_32Bits();
+
+            this.GameDateTimeUTC = DateTime.FromFileTimeUtc(FileTime.FileTime_To_Long(FTime));
+        }
+
+        public void Parse_Color_Info(string ID)
+        {
+            int PlayerNum = Get_Player_Number_From_ID(ID);
+            this.Read_Garbage();
+
+            this.PlayerColors[PlayerNum - 1] = this.Read_Byte();
+        }
+
         public void Parse_Credits_Info(string ID)
         {
             int PlayerNum = Get_Player_Number_From_ID(ID);
-            this.Read_32Bits(); // Read garbage
+            this.Read_Garbage();
 
             this.PlayerCredits[PlayerNum - 1] = this.Read_32Bits();
         }
@@ -320,7 +361,7 @@ namespace ra303pStatsDumpParser
 
             int AlignRead = 4 - (Length % 4);
             if (AlignRead == 4) { AlignRead = 0; }
-            Bin.ReadBytes( AlignRead ); // Read for 4 byte alignment
+            Bin.ReadBytes(AlignRead); // Read for 4 byte alignment
 
             Console.WriteLine("Length = {0}, AlignRead = {1}", Length, AlignRead);
             Pos += AlignRead + 4 + Length;
@@ -341,6 +382,12 @@ namespace ra303pStatsDumpParser
         {
             Pos += 4;
             return Bin.ReadInt32();
+        }
+
+        public uint Read_Unsigned_32Bits()
+        {
+            Pos += 4;
+            return Bin.ReadUInt32();
         }
 
         public int Read_Byte()
@@ -378,6 +425,14 @@ namespace ra303pStatsDumpParser
             this.PlayerSides[PlayerNum - 1] = Parse_Short_String();
 
             this.QuitPlayerNumHelper = PlayerNum; // To help parsing QUIT per player
+        }
+
+        public void Parse_Name_Info(String ID)
+        {
+            int PlayerNum = Get_Player_Number_From_ID(ID);
+
+            this.PlayerNames[PlayerNum - 1] = Parse_String();
+
         }
 
         public CratesCollectedStruct Parse_Crates_Collected_For_Player()
@@ -454,84 +509,130 @@ namespace ra303pStatsDumpParser
             }
         }
     }
+
+    public struct FileTime
+    {
+        public uint dwLowDateTime;
+        public uint dwHighDateTime;
+
+        public static long FileTime_To_Long(FileTime fileTime)
+        {
+            long returnedLong;
+            // Convert 4 high-order bytes to a byte array
+            byte[] highBytes = BitConverter.GetBytes(fileTime.dwHighDateTime);
+            // Resize the array to 8 bytes (for a Long)
+            Array.Resize(ref highBytes, 8);
+
+            // Assign high-order bytes to first 4 bytes of Long
+            returnedLong = BitConverter.ToInt64(highBytes, 0);
+            // Shift high-order bytes into position
+            returnedLong = returnedLong << 32;
+            // Or with low-order bytes
+            returnedLong = returnedLong | fileTime.dwLowDateTime;
+            // Return long 
+            return returnedLong;
+        }
+    }
+
+    [Serializable()]
+    public class StatsDumpLengthException : StatsDumpException
+    {
+        public StatsDumpLengthException() : base() { }
+        public StatsDumpLengthException(string message) : base(message) { }
+        public StatsDumpLengthException(string message, System.Exception inner) : base(message, inner) { }
+
+        // A constructor is needed for serialization when an 
+        // exception propagates from a remoting server to the client.  
+        protected StatsDumpLengthException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) { }
+    }
+
+    [Serializable()]
+    public class StatsDumpException : System.Exception
+    {
+        public StatsDumpException() : base() { }
+        public StatsDumpException(string message) : base(message) { }
+        public StatsDumpException(string message, System.Exception inner) : base(message, inner) { }
+
+        // A constructor is needed for serialization when an 
+        // exception propagates from a remoting server to the client.  
+        protected StatsDumpException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) { }
+    }
+
+    public class BigEndianReader
+    {
+        public BigEndianReader(BinaryReader baseReader)
+        {
+            mBaseReader = baseReader;
+        }
+
+        public short ReadInt16()
+        {
+            return BitConverter.ToInt16(ReadBigEndianBytes(2), 0);
+        }
+
+        public ushort ReadUInt16()
+        {
+            return BitConverter.ToUInt16(ReadBigEndianBytes(2), 0);
+        }
+
+        public uint ReadUInt32()
+        {
+            return BitConverter.ToUInt32(ReadBigEndianBytes(4), 0);
+        }
+
+        public int ReadInt32()
+        {
+            return BitConverter.ToInt32(ReadBigEndianBytes(4), 0);
+        }
+
+        public byte[] ReadBigEndianBytes(int count)
+        {
+            byte[] bytes = new byte[count];
+            for (int i = count - 1; i >= 0; i--)
+                bytes[i] = mBaseReader.ReadByte();
+
+            return bytes;
+        }
+
+        public byte[] ReadBytes(int count)
+        {
+            return mBaseReader.ReadBytes(count);
+        }
+
+        public void Close()
+        {
+            mBaseReader.Close();
+        }
+
+        public Stream BaseStream
+        {
+            get { return mBaseReader.BaseStream; }
+        }
+
+        private BinaryReader mBaseReader;
+    }
 }
 
-[Serializable()]
-public class StatsDumpLengthException : StatsDumpException
+public struct CratesCollectedStruct
 {
-    public StatsDumpLengthException() : base() { }
-    public StatsDumpLengthException(string message) : base(message) { }
-    public StatsDumpLengthException(string message, System.Exception inner) : base(message, inner) { }
-
-    // A constructor is needed for serialization when an 
-    // exception propagates from a remoting server to the client.  
-    protected StatsDumpLengthException(System.Runtime.Serialization.SerializationInfo info,
-        System.Runtime.Serialization.StreamingContext context) { }
-}
-
-[Serializable()]
-public class StatsDumpException : System.Exception
-{
-    public StatsDumpException() : base() { }
-    public StatsDumpException(string message) : base(message) { }
-    public StatsDumpException(string message, System.Exception inner) : base(message, inner) { }
-
-    // A constructor is needed for serialization when an 
-    // exception propagates from a remoting server to the client.  
-    protected StatsDumpException(System.Runtime.Serialization.SerializationInfo info,
-        System.Runtime.Serialization.StreamingContext context) { }
-}
-
-public class BigEndianReader
-{
-    public BigEndianReader(BinaryReader baseReader)
-    {
-        mBaseReader = baseReader;
-    }
-
-    public short ReadInt16()
-    {
-        return BitConverter.ToInt16(ReadBigEndianBytes(2), 0);
-    }
-
-    public ushort ReadUInt16()
-    {
-        return BitConverter.ToUInt16(ReadBigEndianBytes(2), 0);
-    }
-
-    public uint ReadUInt32()
-    {
-        return BitConverter.ToUInt32(ReadBigEndianBytes(4), 0);
-    }
-
-    public int ReadInt32()
-    {
-        return BitConverter.ToInt32(ReadBigEndianBytes(4), 0);
-    }
-
-    public byte[] ReadBigEndianBytes(int count)
-    {
-        byte[] bytes = new byte[count];
-        for (int i = count - 1; i >= 0; i--)
-            bytes[i] = mBaseReader.ReadByte();
-
-        return bytes;
-    }
-
-    public byte[] ReadBytes(int count)
-    {
-        return mBaseReader.ReadBytes(count);
-    }
-
-    public void Close()
-    {
-        mBaseReader.Close();
-    }
-
-    public Stream BaseStream
-    {
-        get { return mBaseReader.BaseStream; }
-    }
-
-    private BinaryReader mBaseReader;
-}
+    public int MoneyCrates;
+    public int UnitCrates;
+    public int ParabombCrates;
+    public int HealCrates;
+    public int StealthCrates;
+    public int ExplosionCrates;
+    public int NapalmDeathCrates;
+    public int SquadCrates;
+    public int MapReshroud;
+    public int MapRevealCrates;
+    public int SonarPulseCrates;
+    public int ArmorUpgradeCrates;
+    public int SpeedUpgradeCrates;
+    public int FirepowerUpgradeCrates;
+    public int OneShotNukeCrates;
+    public int TimeQuakeCrates;
+    public int IronCurtainCrates;
+    public int ChronoVortexCrates;
+};
